988f6f53f065a5b4099276e8a5c116bbec6cc143
Unknown <aspavlychev@yandex.ru>
2021-04-26 09:25:12 +0300

    Реализовал метод nextLine и "ленивый" список funcSeq
    Проведен анализ алгоритма для расчета последовательности, изучена работа с головой одно-связанного списка List и рекурсией, построена бесконечная последовательность LazyList, дописаны unit-тесты

--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -17,9 +17,27 @@ object task_seq_riddle {
    *
    * 1. Реализуйте функцию генерирующую след последовательность из текущей
    * */
+   def nextLine(currentLine: List[Int]): List[Int] = currentLine match {
+     //расчет головы
+     case Nil => Nil
+     case 1 :: Nil => 1 :: 1 :: Nil
+     case 1 :: 1 :: Nil => 2 :: 1 :: Nil
+     case 2 :: 1 :: Nil => 1 :: 2 :: 1 :: 1 :: Nil
+     case 1 :: 2 :: 1 :: 1 :: Nil => 1 :: 1 :: 1 :: 2 :: 2 :: 1 :: Nil
+     case 1 :: x :: 1 :: 1 :: y :: z :: xs if x > 1 && x - y == 1 && y == z => 1 :: 1 :: 1 :: x :: x :: 1 :: y :: y :: nextLine(0 :: xs)
+     case 1 :: 1 :: 1 :: x :: xs if x > 1 => x + 1 :: 1 :: x :: x :: nextLine(0 :: xs)
+     case x :: 1 :: y :: z :: xs if x > 1 && x - y == 1 && y == z => 1 :: x :: 1 :: 1 :: y :: y :: nextLine(0 :: xs)
+     //расчет хвоста - анализируем первые 4 элемента (рекурсия вниз - 0 используем как маркер)
+     case 0 :: Nil => Nil
+     case 0 :: 1 :: Nil => 1 :: Nil
+     case 0 :: 1 :: 1 :: Nil => 2 :: 1 :: Nil
+     case 0 :: 2 :: 1 :: Nil => 1 :: 1 :: Nil
+     case 0 :: x :: 1 :: y :: z :: xs if x > 1 && y == x - 1 && y == z => 1 :: 1 :: y :: y :: nextLine(0 :: xs)
+     case 0 :: 1 :: 1 :: y :: z :: xs if y > 1 && y == z => y + 1 :: 1 :: y :: y :: nextLine(0 :: xs)
+   }
 
-  def nextLine(currentLine: List[Int]): List[Int] =
-    task"Реализуйте функцию генерирующую след последовательность из текущей"()
+
+  //task"Реализуйте функцию генерирующую след последовательность из текущей"()
 
   /**
    * 2. Реализуйте ленивый список, который генерирует данную последовательность
@@ -28,7 +46,9 @@ object task_seq_riddle {
    * lazy val funSeq: LazyList[List[Int]]  ...
    *
    */
+  lazy val funSeq: LazyList[List[Int]] = {
+    def fill(list: List[Int]): LazyList[List[Int]] = LazyList.cons(list, fill(nextLine(list)))
+    fill(List(1))
+  }
 
-  val funSeq: LazyList[List[Int]] =
-    task"Реализуйте ленивый список, который генерирует данную последовательность"()
 }
