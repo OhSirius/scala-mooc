f3a40293f6bef08f418d7fdd81d1d8b85d9ed60e
Unknown <aspavlychev@yandex.ru>
2021-05-03 13:22:54 +0300

    Реализовал метод fullSequence
    Использовал метод foldRight для итерации списка Future и конкатенации  результатов в правильном порядке, также добавил ClassTag для паттерн-матчинга по типу A

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -3,6 +3,7 @@ package homeworks.futures
 import homeworks.HomeworksUtils.TaskSyntax
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.reflect.ClassTag
 
 object task_futures_sequence {
 
@@ -15,11 +16,20 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
-  def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+  def fullSequence[A:ClassTag](futures: List[Future[A]])
+                              (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
+      futures.foldRight(Future.successful((List.empty[A], List.empty[Throwable])))((f, coll) =>
+      for {
+        v <- f.recover { case e: Throwable => e }
+        (values, errors) <- coll
+      } yield v match {
+        case v: A => (v :: values, errors)
+        case e: Throwable => (values, e :: errors)
+      })
+
 }
